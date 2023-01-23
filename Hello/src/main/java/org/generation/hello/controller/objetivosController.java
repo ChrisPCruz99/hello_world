c8980f73f93b5e3cc84ac2b3060dc73394686ae0
unknown <christian.pcruz99@gmail.com>
2023-01-23 13:20:35 -0300

    V1

---
+++ Hello/src/main/java/org/generation/hello/controller/objetivosController.java
@@ -0,0 +1,15 @@
+package org.generation.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController // Informa que é um controller
+@RequestMapping("/objetivos") //Informa o endereço da web
+
+public class objetivosController {
+	@GetMapping //Pega informação do método e exibe
+	public String hello() {
+		return "Aprender as funções do STS, entender melhor as bibliotecas"
+				+ "e trabalhar no projeto integrador.";	
+			}
+}
